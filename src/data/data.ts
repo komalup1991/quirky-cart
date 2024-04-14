edc13894536de31c6ecc0378a665b35f47384267
Komal12 <upadhyay.ko@northeastern.edu>
2024-04-14 16:50:06 -0700

    Slider

---
+++ src/data/data.ts
@@ -0,0 +1,23 @@
+export const transItems = [
+    {
+        id: 1,
+        title: "SPRING SALE",
+        desc: "Explore our latest spring-inspired mug designs, featuring blooming flowers, chirping birds, and vibrant pastel colors. Each mug is crafted with care to bring a smile to your face with every sip.",
+        bg: "6e7679",
+        img: "https://images.unsplash.com/photo-1662873689471-9441b004c310?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cXVpcmt5JTIwbXVnc3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    {
+        id: 2,
+        title: "WINTER SALE",
+        desc: "Choose from our eco-conscious mug selection, made from sustainable materials like bamboo, recycled glass, and ceramic. Enjoy your favorite drinks while reducing your environmental footprint!",
+        bg: "#f5fafd",
+        img: "https://images.unsplash.com/photo-1691293363987-dec1a3b22dbd?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+        id: 3,
+        title: "SPECIAL SALE",
+        desc: "Take advantage of our exclusive spring discounts and promotions! From buy-one-get-one offers to bundle deals, there's something for every mug lover to enjoy.",
+        bg: "#f5fafd",
+        img: "https://images.unsplash.com/photo-1590005194861-af70fa800956?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzN8fG5pY2hlJTIwbXVnfGVufDB8fDB8fHww",
+    },  
+];
--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Notify from '../components/Notify'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
     <div>
     <Notify/>
     <Navbar/>   
+    <Slider/>
     </div>
   )
 }
