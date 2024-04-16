334afac20f0eba4337efcf34dbb5038d165790c5
Komal12 <upadhyay.ko@northeastern.edu>
2024-04-16 10:46:47 -0700

    payment

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,27 @@ import SingleProductDetail from './products/SingleProductDetail'
 import Register from './auth/Register'
 import Login from './auth/Login'
 import ShoppingCart from './cart/ShoppingCart'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-const App = () => {
-  return <Home/>;
+import Payment from './payment/Payment'
+import PaymentSuccess from './payment/PaymentSuccess'
+  const App = () => {
+      // return <Home/>;
   // return <AllProducts/>;
   // return <SingleProductDetail/>;
   // return <Register/>;
   // return <Login/>;
   // return<ShoppingCart/>;
+    return (
+      <BrowserRouter>
+        <Routes>  
+          <Route path="/payment" element={<Payment />} />
+          <Route path="/paymentSuccess" element={<PaymentSuccess />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
   
-}
+  
 
 export default App
