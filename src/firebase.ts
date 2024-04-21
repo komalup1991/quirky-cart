cb5a2e463172776235fe70e39833662f4a844ed9
Komal12 <upadhyay.ko@northeastern.edu>
2024-04-21 09:29:44 -0700

    remove key

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,13 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBckSwqdHCGVTQdDmyvAOwHnCho8FXnYL0",
+  apiKey: FIREBASE_API_KEY,
   authDomain: "quirkycart-bb4aa.firebaseapp.com",
   projectId: "quirkycart-bb4aa",
   storageBucket: "quirkycart-bb4aa.appspot.com",
